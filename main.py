b951ae95a9f7413b40c54551c33692adcb129af0
yannramusat <yann.ramusat@ens.fr>
2023-07-11 17:47:45 +0200

    specs

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from neo4j import GraphDatabase
 #   Neo4j Community Edition 5.9.0
 #   Neo4j Python Driver 5.9.0
 # Hardware specs:
+#   HP EliteBook 840 G3 (L3C67AV)
 #   Intel(R) Core(TM) i7-6600U CPU @ 2.60GHz
 #   32GiB system memory (2133 MHz)
 
