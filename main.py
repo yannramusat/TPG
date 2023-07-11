2c99fc3a58ab38db9f35daee82291b19030e7f63
yannramusat <yann.ramusat@ens.fr>
2023-07-11 17:44:42 +0200

    add specs

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-# create a sandbox and copy the code from the Python panel
+from neo4j import GraphDatabase
 
-from neo4j import GraphDatabase, basic_auth
-
-# connect to a local neo4j community edition instance
-# this instance should be configured with default settings except:
-#    #server.directories.import=import
-#    dbms.security.auth_enabled=false
-#    #dbms.security.allow_csv_import_from_file_urls=true
-# hence no credentials are needed and we can LOAD from CSV files 
+# connect to a local Neo4j Community Edition instance
+# this instance should be configured with default settings but:
+#   #server.directories.import=import
+#   dbms.security.auth_enabled=false
+#   #dbms.security.allow_csv_import_from_file_urls=true
+# hence no credentials are needed and we can LOAD CSV from files 
 # located anywhere in the filesystem
 
 # start the server by typing: `sudo /opt/neo4j/bin/neo4j start`
 # do this instead of `sudo -u neo4j /opt/neo4j/bin/neo4j start` 
-# to avoid permission errors
+# in order to avoid permission errors
+
+# Software specs:
+#   java-openJDK-17
+#   Neo4j Community Edition 5.9.0
+#   Neo4j Python Driver 5.9.0
+# Hardware specs:
+#   Intel(R) Core(TM) i7-6600U CPU @ 2.60GHz
+#   32GiB system memory (2133 MHz)
+
 driver = GraphDatabase.driver(
         "bolt://localhost:7687",
         auth=None)
