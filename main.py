2ab585b12d06a540da80df55f1a8d5a954289c84
yannramusat <yann.ramusat@ens.fr>
2023-07-12 16:51:42 +0200

    Encapsulate functions

--- main.py
+++ main.py
@@ -3,39 +3,69 @@
 from neo4j import GraphDatabase
 
 class App(object):
-    def __init__(self, uri, database=None):
+    def __init__(self, uri, database, verbose=False):
         self.driver = GraphDatabase.driver(
-                        uri,
-                        auth=None)
+                uri, 
+                auth=None)
         self.database = database
+        self.verbose = verbose
 
     def close(self):
         self.driver.close
 
-    def flush(self):
-        flush_query = '''MATCH (n) DETACH DELETE(n)'''
-        with self.driver.session() as session:
-            result = self.driver.execute_query(flush_query,database=self.database)
-            print(result)
+    def print_query_stats(self, records, summary, keys):
+        print("The query `{query}` returned {records_count} records in {time} ms.".format(
+            query=summary.query, 
+            records_count=len(records),
+            time=summary.result_available_after,
+            ))
 
-cypher_query = '''
-MATCH (n)
-RETURN COUNT(n) as count
-LIMIT $limit
-'''
+    def flush_database(self):
+        flush_query = """
+        MATCH (n) DETACH DELETE(n)
+        """
+        records, summary, keys = self.driver.execute_query(
+                flush_query,
+                database=self.database)
+        if(self.verbose):
+            self.print_query_stats(records, summary, keys)
+        print(f"Del:    Deleted {summary.counters.nodes_deleted} nodes, deleted {summary.counters.relationships_deleted} relationships, completed after {summary.result_available_after} ms.")
+
+    def populate_with_csv(self, path_to_csv_file, fieldterminator='|', label="test"):
+        populate_query = """
+        LOAD CSV FROM 'file:///home/yann/research/ibench/build/ibench/try/address.csv' as row FIELDTERMINATOR '|'
+        MERGE (n:Address {zip: row[1], city: row[2]})
+        """
+        records, summary, keys = self.driver.execute_query(
+                populate_query,
+                )
+        if(self.verbose):
+            self.print_query_stats(records, summary, keys)
+        print(f"CSV:    Added {summary.counters.labels_added} labels, created {summary.counters.nodes_created} nodes, " 
+              f"set {summary.counters.properties_set} properties, created {summary.counters.relationships_created} relationships, completed after {summary.result_available_after} ms.")
+
+    def output_all_nodes(self):
+        count_all_query = """
+        MATCH (n)
+        RETURN COUNT(n) as count
+        """
+        records, summary, keys = self.driver.execute_query(
+                count_all_query,
+                )
+        if(self.verbose):
+            self.print_query_stats(records, summary, keys)
+        print(f"All:    There is currently {records[0]['count']} node(s) in the database.")
 
 if __name__ == "__main__":
     scheme = "bolt"
     hostname = "localhost"
     port = "7687"
     uri = f"{scheme}://{hostname}:{port}"
-    app = App(uri, "neo4j")
-    with app.driver.session(database="neo4j") as session:
-        results = session.read_transaction(
-                    lambda tx: tx.run(cypher_query,
-                                        limit=10).data())
-        for record in results:
-            print(record['count'])
-    app.flush()
+    app = App(uri, "neo4j", verbose=False)
 
+    prefix = "file:///home/yann/ibench/build/ibench/"
+    app.populate_with_csv(prefix+"try/measure_le_0_nl0_ce0_address.csv")
+    app.output_all_nodes()
+    app.flush_database()
+    app.output_all_nodes()
     app.close()
