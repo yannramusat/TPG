c8db7874aa2182783cd535a85622940f61df5c4b
yannramusat <yann.ramusat@ens.fr>
2023-07-31 11:03:01 +0200

    Add a Naive alternative for the scenario PersonAddress

--- scenarios/person_address.py
+++ scenarios/person_address.py
@@ -2,6 +2,69 @@ import os
 from app import App
 from structures import InputRelation, InputSchema, TransformationRule, Scenario
 
+
+class PersonAddressScenarioNaive(Scenario):
+    def __init__(self, prefix, size = 100, lstring = 5):
+        ## building scenario
+        # csv#1
+        rel_address_cmd = "MERGE (n:Address {zip: row[1], city: row[2]})"
+        param_string = "personaddress/address"+str(size)+"-"+str(lstring)+".csv"
+        rel_address = InputRelation(os.path.join(prefix, param_string), rel_address_cmd)
+        # csv#2
+        rel_person_cmd = "MERGE (n:Person {name: row[1], address: row[2]})"
+        param_string = "personaddress/person"+str(size)+"-"+str(lstring)+".csv"
+        rel_person = InputRelation(os.path.join(prefix, param_string), rel_person_cmd)
+        # source schema
+        self.schema = InputSchema([rel_address, rel_person])
+
+        # rule#1 using our framework
+        rule1 = TransformationRule("""
+        MATCH (a:Address)
+        MERGE (x {
+            _id: "(Person2:" + a.zip + "," + a.city + ")"
+        })
+        SET x:Person2, 
+            x.address = a.zip
+        MERGE (y {
+            _id: "(Address2:" + elementId(a) + ")"
+        })
+        SET y:Address2, 
+            y.zip = a.zip, 
+            y.city = a.city
+        MERGE (x)-[v:livesAt {
+            _id: "(livesAt:" + elementId(x) + "," + elementId(y) + ")"
+        }]->(y)
+        """)
+        # rule#2 using our framework
+        rule2 = TransformationRule("""
+        MATCH (p:Person)
+        MATCH (a:Address)
+        WHERE p.address = a.zip
+        MERGE (x { 
+            _id: "(Person2:" + elementId(p) + ")"
+        })
+        SET x:Person2,
+            x.name = p.name,
+            x.address = p.address
+        MERGE (y { 
+            _id: "(Address2:" + elementId(a) + ")"
+        })
+        SET y:Address2,
+            y.zip = a.zip,
+            y.city = a.city
+        MERGE (x)-[v:livesAt {
+            _id: "(livesAt:" + elementId(x) + "," + elementId(y) + ")"
+        }]->(y)
+        """)
+        # transformation rules
+        self.rules = [rule1, rule2]
+
+    def addIndexes(self, app, stats=False):
+        pass
+    
+    def destroyIndexes(self, app, stats=False):
+        pass
+
 class PersonAddressScenario(Scenario):
     def __init__(self, prefix, size = 100, lstring = 5):
         ## building scenario
@@ -15,11 +78,19 @@ class PersonAddressScenario(Scenario):
         rel_person = InputRelation(os.path.join(prefix, param_string), rel_person_cmd)
         # source schema
         self.schema = InputSchema([rel_address, rel_person])
+
         # rule#1 using our framework
         rule1 = TransformationRule("""
         MATCH (a:Address)
-        MERGE (x:Person2 { _id: "(Person2:" + a.zip + "," + a.city + ")", address: a.zip })
-        MERGE (y:Address2 { _id: "(Address2:" + elementId(a) + ")", zip: a.zip, city: a.city})
+        MERGE (x:Person2 {
+            _id: "(Person2:" + a.zip + "," + a.city + ")", 
+            address: a.zip
+        })
+        MERGE (y:Address2 {
+            _id: "(Address2:" + elementId(a) + ")", 
+            zip: a.zip, 
+            city: a.city
+        })
         MERGE (x)-[v:livesAt {
             _id: "(livesAt:" + elementId(x) + "," + elementId(y) + ")"
         }]->(y)
@@ -29,14 +100,22 @@ class PersonAddressScenario(Scenario):
         MATCH (p:Person)
         MATCH (a:Address)
         WHERE p.address = a.zip
-        MERGE (x:Person2 { _id: "(Person2:" + elementId(p) + ")", name: p.name, address: p.address })
-        MERGE (y:Address2 { _id: "(Address2:" + elementId(a) + ")" , zip: a.zip, city: a.city})
+        MERGE (x:Person2 { 
+            _id: "(Person2:" + elementId(p) + ")", 
+            name: p.name, 
+            address: p.address 
+        })
+        MERGE (y:Address2 { 
+            _id: "(Address2:" + elementId(a) + ")", 
+            zip: a.zip, 
+            city: a.city
+        })
         MERGE (x)-[v:livesAt {
             _id: "(livesAt:" + elementId(x) + "," + elementId(y) + ")"
         }]->(y)
         """)
         # transformation rules
-        self.rules = [rule1, rule2] 
+        self.rules = [rule1, rule2]
 
     def addIndexes(self, app, stats=False):
         # index on address2
