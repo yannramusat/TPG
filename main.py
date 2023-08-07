a751cc0738a3b6a71d9fe882bc7f749772e89ed1
yannramusat <yann.ramusat@ens.fr>
2023-08-07 12:40:54 +0200

    Factorizing exp. in 'Figure's classes

--- main.py
+++ main.py
@@ -2,6 +2,156 @@
 
 from app import App
 
+class FigureComparisonIndexesPersonAddress(object):
+    def __init__(self):
+        self.x = []
+        self.results_Sep_NI_RI = []
+        self.results_Sep_NI = []
+        self.results_Sep_RI = []
+        self.results_Sep = []
+        self.results_Plain_NI_RI = []
+        self.results_Plain_NI = []
+        self.results_Plain_RI = []
+        self.results_Plain = []
+        self.results_CDoverSI_NI_RI = []
+        self.results_CDoverSI_NI = []
+        self.results_CDoverSI_RI = []
+        self.results_CDoverSI = []
+        self.results_CDoverPlain_NI_RI = []
+        self.results_CDoverPlain_NI = []
+        self.results_CDoverPlain_RI = []
+        self.results_CDoverPlain = []
+
+    def compute(self, values=[], nbLaunches=1, showStats=True, nodeindexes=True, relIndexes=True):
+        self.x = values
+        # execute the alternative implementation of the scenario PersonAddress with Separate indexes
+        from scenarios.personaddress import PersonAddressScenarioSeparateIndexes
+        for i in x:
+            scenario = PersonAddressScenarioSeparateIndexes(prefix, size=i)
+            self.results_Sep_NI_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=True))
+        for i in x:
+            scenario = PersonAddressScenarioSeparateIndexes(prefix, size=i)
+            self.results_Sep_NI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
+        for i in x:
+            scenario = PersonAddressScenarioSeparateIndexes(prefix, size=i)
+            self.results_Sep_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=True))
+        for i in x:
+            scenario = PersonAddressScenarioSeparateIndexes(prefix, size=i)
+            self.results_Sep.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=False))
+        # execute the plain implementation of the scenario PersonAddress
+        from scenarios.personaddress import PersonAddressScenarioPlain
+        for i in x:
+            scenario = PersonAddressScenarioPlain(prefix, size=i)
+            self.results_Plain_NI_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=True))
+        for i in x:
+            scenario = PersonAddressScenarioPlain(prefix, size=i)
+            self.results_Plain_NI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
+        for i in x:
+            scenario = PersonAddressScenarioPlain(prefix, size=i)
+            self.results_Plain_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=True))
+        for i in x:
+            scenario = PersonAddressScenarioPlain(prefix, size=i)
+            self.results_Plain.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=False))
+        # execute the alternative implementation with Conflict Detection of the scenario PersonAddress based on Separate index
+        from scenarios.personaddress import PersonAddressScenarioCDoverSI
+        for i in x:
+            scenario = PersonAddressScenarioCDoverSI(prefix, size=i)
+            self.results_CDoverSI_NI_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=True))
+        for i in x:
+            scenario = PersonAddressScenarioCDoverSI(prefix, size=i)
+            self.results_CDoverSI_NI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
+        for i in x:
+            scenario = PersonAddressScenarioCDoverSI(prefix, size=i)
+            self.results_CDoverSI_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=True)) 
+        for i in x:
+            scenario = PersonAddressScenarioCDoverSI(prefix, size=i)
+            self.results_CDoverSI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=False))
+        # execute the alternative implementation with Conflict Detection of the scenario PersonAddress based on Plain implementation
+        from scenarios.personaddress import PersonAddressScenarioCDoverPlain
+        for i in x:
+            scenario = PersonAddressScenarioCDoverPlain(prefix, size=i)
+            self.results_CDoverPlain_NI_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=True))
+        for i in x:
+            scenario = PersonAddressScenarioCDoverPlain(prefix, size=i)
+            self.results_CDoverPlain_NI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
+        for i in x:
+            scenario = PersonAddressScenarioCDoverPlain(prefix, size=i)
+            self.results_CDoverPlain_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=True))
+        for i in x:
+            scenario = PersonAddressScenarioCDoverPlain(prefix, size=i)
+            self.results_CDoverPlain.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=False))
+    
+    def plot(self):
+        # plot results using matplotlib
+        import matplotlib.pyplot as plt
+        import numpy as np
+        # Figure for exhaustive comparison of indexes | PersonAddress scenario
+        fig1, axs = plt.subplots(2, 2, layout="constrained") 
+        fig1.suptitle("PersonAddress scenario", fontsize=16)
+        # Axes Separate indexes
+        axs[0, 0].plot(x, self.results_Sep_NI_RI, label="Indexes on Nodes and Relations")
+        axs[0, 0].plot(x, self.results_Sep_NI, label="Indexes on Nodes only")
+        axs[0, 0].plot(x, self.results_Sep_RI, label="Indexes on Relations only")
+        axs[0, 0].plot(x, self.results_Sep, label="Without indexes")
+        axs[0, 0].set_title("Separate indexes alternative implementation")
+        axs[0, 0].set_xlabel("number of rows in each input relation")
+        axs[0, 0].set_ylabel("time (ms)")
+        axs[0, 0].set_yscale("log")
+        axs[0, 0].legend()
+        # Axes Plain implementation
+        axs[0, 1].plot(x, self.results_Plain_NI_RI, label="Indexes on Nodes and Relations")
+        axs[0, 1].plot(x, self.results_Plain_NI, label="Indexes on Nodes only")
+        axs[0, 1].plot(x, self.results_Plain_RI, label="Indexes on Relations only") 
+        axs[0, 1].plot(x, self.results_Plain, label="Without indexes")
+        axs[0, 1].set_title("Plain implementation")
+        axs[0, 1].set_xlabel("number of rows in each input relation")
+        axs[0, 1].set_ylabel("time (ms)")
+        axs[0, 1].set_yscale("log")
+        axs[0, 1].legend()
+        # Axes Conflict Detection over Separate indexes
+        axs[1, 0].plot(x, self.results_CDoverSI_NI_RI, label="Indexes on Nodes and Relations")
+        axs[1, 0].plot(x, self.results_CDoverSI_NI, label="Indexes on Nodes only")
+        axs[1, 0].plot(x, self.results_CDoverSI_RI, label="Indexes on Relations only") 
+        axs[1, 0].plot(x, self.results_CDoverSI, label="Without indexes")
+        axs[1, 0].set_title("Conflict Detection over Separate indexes")
+        axs[1, 0].set_xlabel("number of rows in each input relation")
+        axs[1, 0].set_ylabel("time (ms)")
+        axs[1, 0].set_yscale("log")
+        axs[1, 0].legend()
+        # Axes Conflict Detection over Plain
+        axs[1, 1].plot(x, self.results_CDoverPlain_NI_RI, label="Indexes on Nodes and Relations")
+        axs[1, 1].plot(x, self.results_CDoverPlain_NI, label="Indexes on Nodes only")
+        axs[1, 1].plot(x, self.results_CDoverPlain_RI, label="Indexes on Relations only") 
+        axs[1, 1].plot(x, self.results_CDoverPlain, label="Without indexes")
+        axs[1, 1].set_title("Conflict Detection over Plain implementation")
+        axs[1, 1].set_xlabel("number of rows in each input relation")
+        axs[1, 1].set_ylabel("time (ms)")
+        axs[1, 1].set_yscale("log")
+        axs[1, 1].legend()
+
+    def print_cmd(self):
+        print("Figure for exhaustive comparison of indexes | PersonAddress scenario")
+        print("# Separate indexes alternative")
+        print(f"{self.results_Sep_NI_RI=}")
+        print(f"{self.results_Sep_NI=}")
+        print(f"{self.results_Sep_RI=}")
+        print(f"{self.results_Sep=}")
+        print("# Plain implementation")
+        print(f"{self.results_Plain_NI_RI=}")
+        print(f"{self.results_Plain_NI=}")
+        print(f"{self.results_Plain_RI=}")
+        print(f"{self.results_Plain=}")
+        print("# Conflict Detection over Separate indexes")
+        print(f"{self.results_CDoverSI_NI_RI=}")
+        print(f"{self.results_CDoverSI_NI=}")
+        print(f"{self.results_CDoverSI_RI=}")
+        print(f"{self.results_CDoverSI=}")
+        print("# Conflict Detection over Plain")
+        print(f"{self.results_CDoverPlain_NI_RI=}")
+        print(f"{self.results_CDoverPlain_NI=}")
+        print(f"{self.results_CDoverPlain_RI=}")
+        print(f"{self.results_CDoverPlain=}")
+
 if __name__ == "__main__":
     # app setup
     scheme = "bolt"
@@ -16,136 +166,40 @@ if __name__ == "__main__":
     showStats = True
     nodeIndexes = True
     relIndexes = True
-    x = [100, 200, 500, 1_000] #, 2_000, 5_000, 10_000, 20_000, 50_000, 100_000]
-    y = [100, 200, 500, 1_000, 2_000, 5_000, 10_000] #, 20_000, 50_000, 100_000]
+    x = [100, 200]#, 500, 1_000] #, 2_000, 5_000, 10_000, 20_000, 50_000, 100_000]
+    y = [100, 200]#, 500, 1_000, 2_000, 5_000, 10_000] #, 20_000, 50_000, 100_000]
+
+    figureCompIndPA = FigureComparisonIndexesPersonAddress()
+    figureCompIndPA.compute(values=x, nbLaunches=nbLaunches, showStats=showStats)
     
     # execute the alternative implementation of the scenario PersonAddress with Separate indexes
     from scenarios.personaddress import PersonAddressScenarioSeparateIndexes
-    results_Sep_NI_RI = []
-    for i in x:
-        scenario = PersonAddressScenarioSeparateIndexes(prefix, size=i)
-        results_Sep_NI_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=True))
-    
-    results_Sep_NI = []
     results_Sep_long = []
-    for i in x:
-        scenario = PersonAddressScenarioSeparateIndexes(prefix, size=i)
-        results_Sep_NI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
     for i in y:
         scenario = PersonAddressScenarioSeparateIndexes(prefix, size=i)
         results_Sep_long.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
-    
-    results_Sep_RI = []
-    for i in x:
-        scenario = PersonAddressScenarioSeparateIndexes(prefix, size=i)
-        results_Sep_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=True))
-    
-    results_Sep = []
-    for i in x:
-        scenario = PersonAddressScenarioSeparateIndexes(prefix, size=i)
-        results_Sep.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=False))
-
     # execute the plain implementation of the scenario PersonAddress
     from scenarios.personaddress import PersonAddressScenarioPlain
-    results_Plain_NI_RI = []
-    for i in x:
-        scenario = PersonAddressScenarioPlain(prefix, size=i)
-        results_Plain_NI_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=True))
-    
-    results_Plain_NI = []
     results_Plain_long = []
-    for i in x:
-        scenario = PersonAddressScenarioPlain(prefix, size=i)
-        results_Plain_NI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
     for i in y:
         scenario = PersonAddressScenarioPlain(prefix, size=i)
         results_Plain_long.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
-
-    results_Plain_RI = []
-    for i in x:
-        scenario = PersonAddressScenarioPlain(prefix, size=i)
-        results_Plain_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=True))
-    
-    results_Plain = []
-    for i in x:
-        scenario = PersonAddressScenarioPlain(prefix, size=i)
-        results_Plain.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=False))
-
     # execute the alternative implementation with Conflict Detection of the scenario PersonAddress based on Separate index
     from scenarios.personaddress import PersonAddressScenarioCDoverSI
-    results_CDoverSI_NI_RI = []
-    for i in x:
-        scenario = PersonAddressScenarioCDoverSI(prefix, size=i)
-        results_CDoverSI_NI_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=True))
-    
-    results_CDoverSI_NI = []
     results_CDoverSI_long = []
-    for i in x:
-        scenario = PersonAddressScenarioCDoverSI(prefix, size=i)
-        results_CDoverSI_NI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
     for i in y:
         scenario = PersonAddressScenarioCDoverSI(prefix, size=i)
         results_CDoverSI_long.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
-    
-    results_CDoverSI_RI = []
-    for i in x:
-        scenario = PersonAddressScenarioCDoverSI(prefix, size=i)
-        results_CDoverSI_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=True))
-    
-    results_CDoverSI = []
-    for i in x:
-        scenario = PersonAddressScenarioCDoverSI(prefix, size=i)
-        results_CDoverSI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=False))
-
     # execute the alternative implementation with Conflict Detection of the scenario PersonAddress based on Plain implementation
     from scenarios.personaddress import PersonAddressScenarioCDoverPlain
-    results_CDoverPlain_NI_RI = []
-    for i in x:
-        scenario = PersonAddressScenarioCDoverPlain(prefix, size=i)
-        results_CDoverPlain_NI_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=True))
-    
-    results_CDoverPlain_NI = []
     results_CDoverPlain_long = []
-    for i in x:
-        scenario = PersonAddressScenarioCDoverPlain(prefix, size=i)
-        results_CDoverPlain_NI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
     for i in y:
         scenario = PersonAddressScenarioCDoverPlain(prefix, size=i)
         results_CDoverPlain_long.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=True, relIndex=False))
-    
-    results_CDoverPlain_RI = []
-    for i in x:
-        scenario = PersonAddressScenarioCDoverPlain(prefix, size=i)
-        results_CDoverPlain_RI.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=True))
-    
-    results_CDoverPlain = []
-    for i in x:
-        scenario = PersonAddressScenarioCDoverPlain(prefix, size=i)
-        results_CDoverPlain.append(scenario.run(app, launches=nbLaunches, stats=showStats, nodeIndex=False, relIndex=False))
 
-    # optional printing of the results in the console
     if showStats:
-        print("# Separate indexes alternative")
-        print(f"{results_Sep_NI_RI=}")
-        print(f"{results_Sep_NI=}")
-        print(f"{results_Sep_RI=}")
-        print(f"{results_Sep=}")
-        print("# Plain implementation")
-        print(f"{results_Plain_NI_RI=}")
-        print(f"{results_Plain_NI=}")
-        print(f"{results_Plain_RI=}")
-        print(f"{results_Plain=}")
-        print("# Conflict Detection over Separate indexes")
-        print(f"{results_CDoverSI_NI_RI=}")
-        print(f"{results_CDoverSI_NI=}")
-        print(f"{results_CDoverSI_RI=}")
-        print(f"{results_CDoverSI=}")
-        print("# Conflict Detection over Plain")
-        print(f"{results_CDoverPlain_NI_RI=}")
-        print(f"{results_CDoverPlain_NI=}")
-        print(f"{results_CDoverPlain_RI=}")
-        print(f"{results_CDoverPlain=}")
-
+        figureCompIndPA.print_cmd()
+    
     if showStats:
         print("# Comparison of alternative implementations")
         print(f"{results_Sep_long=}")
@@ -153,53 +207,10 @@ if __name__ == "__main__":
         print(f"{results_CDoverSI_long=}")
         print(f"{results_CDoverPlain_long=}")
 
+    figureCompIndPA.plot()
     # plot results using matplotlib
     import matplotlib.pyplot as plt
     import numpy as np
-
-    # Figure for exhaustive comparison of indexes | PersonAddress scenario
-    fig1, axs = plt.subplots(2, 2, layout="constrained") 
-    fig1.suptitle("PersonAddress scenario", fontsize=16)
-    # Axes Separate indexes
-    axs[0, 0].plot(x, results_Sep_NI_RI, label="Indexes on Nodes and Relations")
-    axs[0, 0].plot(x, results_Sep_NI, label="Indexes on Nodes only")
-    axs[0, 0].plot(x, results_Sep_RI, label="Indexes on Relations only")
-    axs[0, 0].plot(x, results_Sep, label="Without indexes")
-    axs[0, 0].set_title("Separate indexes alternative implementation")
-    axs[0, 0].set_xlabel("number of rows in each input relation")
-    axs[0, 0].set_ylabel("time (ms)")
-    axs[0, 0].set_yscale("log")
-    axs[0, 0].legend()
-    # Axes Plain implementation
-    axs[0, 1].plot(x, results_Plain_NI_RI, label="Indexes on Nodes and Relations")
-    axs[0, 1].plot(x, results_Plain_NI, label="Indexes on Nodes only")
-    axs[0, 1].plot(x, results_Plain_RI, label="Indexes on Relations only") 
-    axs[0, 1].plot(x, results_Plain, label="Without indexes")
-    axs[0, 1].set_title("Plain implementation")
-    axs[0, 1].set_xlabel("number of rows in each input relation")
-    axs[0, 1].set_ylabel("time (ms)")
-    axs[0, 1].set_yscale("log")
-    axs[0, 1].legend()
-    # Axes Conflict Detection over Separate indexes
-    axs[1, 0].plot(x, results_CDoverSI_NI_RI, label="Indexes on Nodes and Relations")
-    axs[1, 0].plot(x, results_CDoverSI_NI, label="Indexes on Nodes only")
-    axs[1, 0].plot(x, results_CDoverSI_RI, label="Indexes on Relations only") 
-    axs[1, 0].plot(x, results_CDoverSI, label="Without indexes")
-    axs[1, 0].set_title("Conflict Detection over Separate indexes")
-    axs[1, 0].set_xlabel("number of rows in each input relation")
-    axs[1, 0].set_ylabel("time (ms)")
-    axs[1, 0].set_yscale("log")
-    axs[1, 0].legend()
-    # Axes Conflict Detection over Plain
-    axs[1, 1].plot(x, results_CDoverPlain_NI_RI, label="Indexes on Nodes and Relations")
-    axs[1, 1].plot(x, results_CDoverPlain_NI, label="Indexes on Nodes only")
-    axs[1, 1].plot(x, results_CDoverPlain_RI, label="Indexes on Relations only") 
-    axs[1, 1].plot(x, results_CDoverPlain, label="Without indexes")
-    axs[1, 1].set_title("Conflict Detection over Plain implementation")
-    axs[1, 1].set_xlabel("number of rows in each input relation")
-    axs[1, 1].set_ylabel("time (ms)")
-    axs[1, 1].set_yscale("log")
-    axs[1, 1].legend()
 
     # Figure for comparing alternative implementations | PersonAddress scenario
     fig2, ax = plt.subplots(layout="constrained")
