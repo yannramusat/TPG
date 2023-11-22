4a9add0ccff46a0b2853bbdaa50a33fe5432fa4e
yannramusat <yann.ramusat@ens.fr>
2023-11-22 15:05:58 +0100

    Add Sep variant for PersonData

--- scenarios/persondatas1.py
+++ scenarios/persondatas1.py
@@ -164,3 +164,139 @@ class PersonDataScenarioS1CDoverPlain(PersonDataScenarioS1Plain):
         """)
         # transformation rules
         self.rules = [rule1]
+
+class PersonDataScenarioS1Sep(PersonDataScenarioS1):
+    def __init__(self, prefix, size = 100, lstring = 5):
+        # input schema
+        super().__init__(prefix, size, lstring)
+
+        # rule#1 using our framework
+        rule1 = TransformationRule("""
+        MATCH (p:Person)
+        MATCH (a:Address) WHERE a.occ = p.name
+        MATCH (pl:Place) WHERE pl.occ = p.name
+        MERGE (x:Person2 { 
+            _id: "(" + elementId(p) + ")" 
+        })
+        SET x.address = p.address
+        MERGE (y:City { 
+            _id: "(" + a.city + ")" 
+        })
+        SET y.city = a.city
+        MERGE (z:Zip {
+            _id: "(" + pl.zip + ")"
+        })
+        SET z.zip = pl.zip
+        MERGE (x)-[ha:HAS_ADDRESS {
+            _id: "(HAS_ADDRESS:" + elementId(x) + "," + elementId(y) + ")"
+        }]->(y)
+        MERGE (x)-[hp:HAS_PLACE {
+            _id: "(HAS_PLACE:" + elementId(x) + "," + elementId(z) + ")"
+        }]->(z)
+        """)
+        # transformation rules
+        self.rules = [rule1]
+
+    def addNodeIndexes(self, app, stats=False):
+        # index on Person2
+        indexPerson2 = """
+        CREATE INDEX idx_Person2 IF NOT EXISTS
+        FOR (n:Person2)
+        ON (n._id)
+        """
+        app.addIndex(indexPerson2, stats)
+
+        # index on City
+        indexCity = """
+        CREATE INDEX idx_City IF NOT EXISTS
+        FOR (n:City)
+        ON (n._id)
+        """
+        app.addIndex(indexCity, stats)
+    
+        # index on Zip
+        indexZip = """
+        CREATE INDEX idx_Zip IF NOT EXISTS
+        FOR (n:Zip)
+        ON (n._id)
+        """
+        app.addIndex(indexZip, stats)
+     
+    def delNodeIndexes(self, app, stats=False):
+        # drop index on Person2
+        dropPerson2 = """
+        DROP INDEX idx_dummy IF EXISTS
+        """
+        app.dropIndex(dropPerson2, stats)
+
+        # drop index on City
+        dropCity = """
+        DROP INDEX idx_City IF EXISTS
+        """
+        app.dropIndex(dropCity, stats)
+
+        # drop index on Zip
+        dropZip = """
+        DROP INDEX idx_Zip IF EXISTS
+        """
+        app.dropIndex(dropZip, stats)
+
+class PersonDataScenarioS1CDoverSep(PersonDataScenarioS1Sep):
+    def __init__(self, prefix, size = 100, lstring = 5):
+        # input schema
+        super().__init__(prefix, size, lstring)
+
+        # rule#1 using our framework
+        rule1 = TransformationRule("""
+        MATCH (p:Person)
+        MATCH (a:Address) WHERE a.occ = p.name
+        MATCH (pl:Place) WHERE pl.occ = p.name
+        MERGE (x:Person2 { 
+            _id: "(" + elementId(p) + ")" 
+        })
+        ON CREATE
+            SET x.address = p.address
+        ON MATCH
+            SET x.address =
+                CASE
+                    WHEN x.address <> p.address
+                        THEN "Conflict detected!"
+                    ELSE
+                        p.address
+                END
+        MERGE (y:City { 
+            _id: "(" + a.city + ")" 
+        })
+        ON CREATE
+            SET y.city = a.city
+        ON MATCH
+            SET y.city =
+                CASE
+                    WHEN y.city <> a.city
+                        THEN "Conflict detected!"
+                    ELSE
+                        a.city
+                END
+        
+        MERGE (z:Zip {
+            _id: "(" + pl.zip + ")"
+        })
+        ON CREATE
+            SET z.zip = pl.zip
+        ON MATCH
+            SET z.zip =
+                CASE
+                    WHEN z.zip <> pl.zip
+                        THEN "Conflict detected!"
+                    ELSE
+                        pl.zip
+                END
+        MERGE (x)-[ha:HAS_ADDRESS {
+            _id: "(HAS_ADDRESS:" + elementId(x) + "," + elementId(y) + ")"
+        }]->(y)
+        MERGE (x)-[hp:HAS_PLACE {
+            _id: "(HAS_PLACE:" + elementId(x) + "," + elementId(z) + ")"
+        }]->(z)
+        """)
+        # transformation rules
+        self.rules = [rule1]
