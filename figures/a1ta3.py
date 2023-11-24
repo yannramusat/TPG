d531a2d02d0392d5e2e40745220d58ba797120ba
yannramusat <yann.ramusat@ens.fr>
2023-11-24 23:38:14 +0100

    Updated fig exhaustive a1ta3

--- figures/a1ta3.py
+++ figures/a1ta3.py
@@ -15,14 +15,14 @@ class FigureComparisonIndexesA1TA3(Figure):
         self.results_Plain_NI = []
         self.results_Plain_RI = []
         self.results_Plain = []
-        self.results_Sep_Shuffled_NI_RI = []
-        self.results_Sep_Shuffled_NI = []
-        self.results_Sep_Shuffled_RI = []
-        self.results_Sep_Shuffled = []
-        self.results_Plain_Shuffled_NI_RI = []
-        self.results_Plain_Shuffled_NI = []
-        self.results_Plain_Shuffled_RI = []
-        self.results_Plain_Shuffled = []
+        self.results_CDoverSI_NI_RI = []
+        self.results_CDoverSI_NI = []
+        self.results_CDoverSI_RI = []
+        self.results_CDoverSI = []
+        self.results_CDoverPlain_NI_RI = []
+        self.results_CDoverPlain_NI = []
+        self.results_CDoverPlain_RI = []
+        self.results_CDoverPlain = []
 
     def compute(self):
         # execute the separate indexes implementation of the scenario A1TA3
@@ -53,82 +53,87 @@ class FigureComparisonIndexesA1TA3(Figure):
         for i in self.x:
             scenario = Amalgam1ToAmalgam3Plain(self.prefix, size=i)
             self.results_Plain.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=False, relIndex=False))
-        # execute the separate indexes implementation (shuffled) of the scenario A1TA3
+        # execute the alternative implementation with Conflict Detection of the scenario A1TA3 based on Separate index
+        from scenarios.a1ta3 import Amalgam1ToAmalgam3CDoverSI
         for i in self.x:
-            scenario = Amalgam1ToAmalgam3SeparateIndexes(self.prefix, size=i)
-            self.results_Sep_Shuffled_NI_RI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=True, relIndex=True, shuffle=True))
+            scenario = Amalgam1ToAmalgam3CDoverSI(self.prefix, size=i)
+            self.results_CDoverSI_NI_RI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=True, relIndex=True))
         for i in self.x:
-            scenario = Amalgam1ToAmalgam3SeparateIndexes(self.prefix, size=i)
-            self.results_Sep_Shuffled_NI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=True, relIndex=False, shuffle=True))
+            scenario = Amalgam1ToAmalgam3CDoverSI(self.prefix, size=i)
+            self.results_CDoverSI_NI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=True, relIndex=False))
         for i in self.x:
-            scenario = Amalgam1ToAmalgam3SeparateIndexes(self.prefix, size=i)
-            self.results_Sep_Shuffled_RI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=False, relIndex=True, shuffle=True)) 
+            scenario = Amalgam1ToAmalgam3CDoverSI(self.prefix, size=i)
+            self.results_CDoverSI_RI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=False, relIndex=True)) 
         for i in self.x:
-            scenario = Amalgam1ToAmalgam3SeparateIndexes(self.prefix, size=i)
-            self.results_Sep_Shuffled.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=False, relIndex=False, shuffle=True))
-        # execute the plain implementation (shuffled) of the scenario A1TA3
+            scenario = Amalgam1ToAmalgam3CDoverSI(self.prefix, size=i)
+            self.results_CDoverSI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=False, relIndex=False))
+        # execute the alternative implementation with Conflict Detection of the scenario A1TA3 based on Plain implementation
+        from scenarios.a1ta3 import Amalgam1ToAmalgam3CDoverPlain
         for i in self.x:
-            scenario = Amalgam1ToAmalgam3Plain(self.prefix, size=i)
-            self.results_Plain_Shuffled_NI_RI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=True, relIndex=True, shuffle=True))
+            scenario = Amalgam1ToAmalgam3CDoverPlain(self.prefix, size=i)
+            self.results_CDoverPlain_NI_RI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=True, relIndex=True))
         for i in self.x:
-            scenario = Amalgam1ToAmalgam3Plain(self.prefix, size=i)
-            self.results_Plain_Shuffled_NI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=True, relIndex=False, shuffle=True))
+            scenario = Amalgam1ToAmalgam3CDoverPlain(self.prefix, size=i)
+            self.results_CDoverPlain_NI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=True, relIndex=False))
         for i in self.x:
-            scenario = Amalgam1ToAmalgam3Plain(self.prefix, size=i)
-            self.results_Plain_Shuffled_RI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=False, relIndex=True, shuffle=True)) 
+            scenario = Amalgam1ToAmalgam3CDoverPlain(self.prefix, size=i)
+            self.results_CDoverPlain_RI.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=False, relIndex=True)) 
         for i in self.x:
-            scenario = Amalgam1ToAmalgam3Plain(self.prefix, size=i)
-            self.results_Plain_Shuffled.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=False, relIndex=False, shuffle=True))
+            scenario = Amalgam1ToAmalgam3CDoverPlain(self.prefix, size=i)
+            self.results_CDoverPlain.append(scenario.run(self.app, launches=self.nbLaunches, stats=self.showStats, nodeIndex=False, relIndex=False))
     
     def plot(self):
         # Figure for exhaustive comparison of indexes | A1TA3 scenario
-        fig1, axs = plt.subplots(2, 2, layout="constrained", figsize=(6,5)) 
+        fig1, axs = plt.subplots(1, 4, layout="constrained", figsize=(12,3)) 
         fig1.suptitle("Amalgam1ToAmalgam3", fontsize=14)
         # Axes Sep implementation
-        axs[0, 0].plot(self.x, self.results_Sep_NI_RI, label="NI_RI", marker="D")
-        axs[0, 0].plot(self.x, self.results_Sep_NI, label="NI", marker="s")
-        axs[0, 0].plot(self.x, self.results_Sep_RI, label="RI", marker="o") 
-        axs[0, 0].plot(self.x, self.results_Sep, label="WI", marker="x")
-        axs[0, 0].set_title("SI")
-        axs[0, 0].set_xlabel("number of nodes of each type")
-        axs[0, 0].set_ylabel("time (ms)")
-        axs[0, 0].set_yscale("log")
-        axs[0, 0].legend(loc="best", ncol=2, fontsize="10")
+        l1 = axs[0].plot(self.x, self.results_Sep_NI_RI, label="NI_RI", marker="D")
+        l2 = axs[0].plot(self.x, self.results_Sep_NI, label="NI", marker="s")
+        l3 = axs[0].plot(self.x, self.results_Sep_RI, label="RI", marker="o") 
+        l4 = axs[0].plot(self.x, self.results_Sep, label="WI", marker="x")
+        axs[0].set_title("SI")
+        axs[0].set_xlabel("number of nodes of each type")
+        axs[0].set_ylabel("time (ms)")
+        axs[0].set_yscale("log")
+        #axs[0].legend(loc="best", ncol=2, fontsize="10")
         # Axes Plain implementation
-        axs[0, 1].plot(self.x, self.results_Plain_NI_RI, label="NI_RI", marker="D")
-        axs[0, 1].plot(self.x, self.results_Plain_NI, label="NI", marker="s")
-        axs[0, 1].plot(self.x, self.results_Plain_RI, label="RI", marker="o") 
-        axs[0, 1].plot(self.x, self.results_Plain, label="WI", marker="x")
-        axs[0, 1].set_title("PI")
-        axs[0, 1].set_xlabel("number of nodes of each type")
-        axs[0, 1].set_ylabel("time (ms)")
-        axs[0, 1].set_yscale("log")
+        axs[1].plot(self.x, self.results_Plain_NI_RI, label="NI_RI", marker="D")
+        axs[1].plot(self.x, self.results_Plain_NI, label="NI", marker="s")
+        axs[1].plot(self.x, self.results_Plain_RI, label="RI", marker="o") 
+        axs[1].plot(self.x, self.results_Plain, label="WI", marker="x")
+        axs[1].set_title("PI")
+        axs[1].set_xlabel("number of nodes of each type")
+        axs[1].set_ylabel("time (ms)")
+        axs[1].set_yscale("log")
         #axs[0, 1].legend()
-        # Axes Sep implementation (shuffled)
-        axs[1, 0].plot(self.x, self.results_Sep_Shuffled_NI_RI, label="NI_RI", marker="D")
-        axs[1, 0].plot(self.x, self.results_Sep_Shuffled_NI, label="NI", marker="s")
-        axs[1, 0].plot(self.x, self.results_Sep_Shuffled_RI, label="RI", marker="o") 
-        axs[1, 0].plot(self.x, self.results_Sep_Shuffled, label="WI", marker="x")
-        axs[1, 0].set_title("SI; Shuffled")
-        axs[1, 0].set_xlabel("number of nodes of each type")
-        axs[1, 0].set_ylabel("time (ms)")
-        axs[1, 0].set_yscale("log")
+        # Axes Conflict Detection over Separate indexes
+        axs[2].plot(self.x, self.results_CDoverSI_NI_RI, label="NI_RI", marker="D")
+        axs[2].plot(self.x, self.results_CDoverSI_NI, label="NI", marker="s")
+        axs[2].plot(self.x, self.results_CDoverSI_RI, label="RI", marker="o") 
+        axs[2].plot(self.x, self.results_CDoverSI, label="WI", marker="x")
+        axs[2].set_title("CD/SI")
+        axs[2].set_xlabel("number of nodes of each type")
+        axs[2].set_ylabel("time (ms)")
+        axs[2].set_yscale("log")
         #axs[1, 0].legend()
-        # Axes Plain implementation (shuffled)
-        axs[1, 1].plot(self.x, self.results_Plain_Shuffled_NI_RI, label="NI_RI", marker="D")
-        axs[1, 1].plot(self.x, self.results_Plain_Shuffled_NI, label="NI", marker="s")
-        axs[1, 1].plot(self.x, self.results_Plain_Shuffled_RI, label="RI", marker="o") 
-        axs[1, 1].plot(self.x, self.results_Plain_Shuffled, label="WI", marker="x")
-        axs[1, 1].set_title("PI; Shuffled")
-        axs[1, 1].set_xlabel("number of nodes of each type")
-        axs[1, 1].set_ylabel("time (ms)")
-        axs[1, 1].set_yscale("log")
+        # Axes Conflict Detection over Plain
+        axs[3].plot(self.x, self.results_CDoverPlain_NI_RI, label="NI_RI", marker="D")
+        axs[3].plot(self.x, self.results_CDoverPlain_NI, label="NI", marker="s")
+        axs[3].plot(self.x, self.results_CDoverPlain_RI, label="RI", marker="o") 
+        axs[3].plot(self.x, self.results_CDoverPlain, label="WI", marker="x")
+        axs[3].set_title("CD/PI")
+        axs[3].set_xlabel("number of nodes of each type")
+        axs[3].set_ylabel("time (ms)")
+        axs[3].set_yscale("log")
         #axs[1, 1].legend()
 
-        axs[0, 0].set_ylim([20, 100_000])
-        axs[0, 1].set_ylim([20, 100_000])
-        axs[1, 0].set_ylim([20, 100_000])
-        axs[1, 1].set_ylim([20, 100_000])
+        labels = ["NI_RI", "NI", "RI", "WI"]
+        fig1.legend([l1, l2, l3, l4], labels=labels, ncol=4, loc="upper right")
+
+        axs[0].set_ylim([20, 100_000])
+        axs[1].set_ylim([20, 100_000])
+        axs[2].set_ylim([20, 100_000])
+        axs[3].set_ylim([20, 100_000])
         plt.savefig("outfigs/FigureExhaustiveIndexesA1TA3.png")
 
     def print_cmd(self):
@@ -143,16 +148,16 @@ class FigureComparisonIndexesA1TA3(Figure):
         print(f"{self.results_Plain_NI=}")
         print(f"{self.results_Plain_RI=}")
         print(f"{self.results_Plain=}")
-        print("# Separate indexes implementation (shuffled)")
-        print(f"{self.results_Sep_Shuffled_NI_RI=}")
-        print(f"{self.results_Sep_Shuffled_NI=}")
-        print(f"{self.results_Sep_Shuffled_RI=}")
-        print(f"{self.results_Sep_Shuffled=}")
-        print("# Plain implementation (shuffled)")
-        print(f"{self.results_Plain_Shuffled_NI_RI=}")
-        print(f"{self.results_Plain_Shuffled_NI=}")
-        print(f"{self.results_Plain_Shuffled_RI=}")
-        print(f"{self.results_Plain_Shuffled=}")
+        print("# Conflict Detection over Separate indexes")
+        print(f"{self.results_CDoverSI_NI_RI=}")
+        print(f"{self.results_CDoverSI_NI=}")
+        print(f"{self.results_CDoverSI_RI=}")
+        print(f"{self.results_CDoverSI=}")
+        print("# Conflict Detection over Plain")
+        print(f"{self.results_CDoverPlain_NI_RI=}")
+        print(f"{self.results_CDoverPlain_NI=}")
+        print(f"{self.results_CDoverPlain_RI=}")
+        print(f"{self.results_CDoverPlain=}")
 
 class FigureComparisonAlternativeApproachesA1TA3(Figure):
     def __init__(self, app, prefix, values=[], nbLaunches=1, showStats=True):
